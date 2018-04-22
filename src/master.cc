1d48bf6ecdd9aeabaefc2667e68cbacf92af38c4
Shea Conlon <sheaconlon@users.noreply.github.com>
2018-04-21 22:40:08 -0700

    add initial master-worker implementation using gRPC

---
+++ src/master.cc
@@ -0,0 +1,61 @@
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+#include "serverless_learn.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+using serverless_learn::Worker;
+using serverless_learn::Chunk;
+using serverless_learn::ReceiveFileResult;
+
+
+class WorkerStub {
+ public:
+  WorkerStub(std::shared_ptr<Channel> channel)
+      : stub_(Worker::NewStub(channel)) {
+    
+  }
+
+  void ReceiveFile() {
+    Chunk chunk;
+    ReceiveFileResult result;
+    ClientContext context;
+
+    std::unique_ptr<ClientWriter<Chunk>> writer(
+        stub_->ReceiveFile(&context, &result));
+    for (int i = 0; i < 100; i++) {
+      chunk.set_data("Hello, world!");
+      if (!writer->Write(chunk)) {
+        break;
+      }
+      std::cout << "sent" << std::endl;
+    }
+    writer->WritesDone();
+    Status status = writer->Finish();
+    if (status.ok()) {
+      std::cout << "ok"
+                << std::endl;
+    } else {
+      std::cout << "not ok" << std::endl;
+    }
+  }
+
+ private:
+  std::unique_ptr<Worker::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  WorkerStub worker(
+      grpc::CreateChannel("localhost:50051",
+                          grpc::InsecureChannelCredentials()));
+  worker.ReceiveFile();
+
+  return 0;
+}
---
+++ src/worker.cc
@@ -0,0 +1,53 @@
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+#include "serverless_learn.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using serverless_learn::Worker;
+using serverless_learn::Chunk;
+using serverless_learn::ReceiveFileResult;
+
+
+class WorkerImpl final : public Worker::Service {
+ public:
+  explicit WorkerImpl() {
+    
+  }
+
+  Status ReceiveFile(ServerContext* context, ServerReader<Chunk>* reader,
+                     ReceiveFileResult* result) override {
+    Chunk chunk;
+
+    while (reader->Read(&chunk)) {
+      std::cout << "received" << std::endl;
+    }
+
+    return Status::OK;
+  }
+
+ private:
+
+};
+
+int main(int argc, char** argv) {
+  std::string server_address("0.0.0.0:50051");
+  WorkerImpl service;
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+
+  return 0;
+}
